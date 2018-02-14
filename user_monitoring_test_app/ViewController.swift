85f7a562769d823a6f3523c3651fc6588de431aa
Yan Malinovsky <yan.malinovsky@gmail.com>
2018-02-14 13:15:33 +0200

    Initial Commit

---
+++ user_monitoring_test_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  user_monitoring_test_app
+//
+//  Created by Yan Malinovsky on 14.02.2018.
+//  Copyright © 2018 Yan Malinovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
