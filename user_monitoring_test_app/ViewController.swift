1c92c938c90e08076b3b94f7df912a0834b66194
Yan Malinovsky <yan.malinovsky@gmail.com>
2018-02-14 15:32:01 +0200

    GoogleMaps - 1

--- user_monitoring_test_app/ViewController.swift
+++ user_monitoring_test_app/ViewController.swift
@@ -11,23 +11,16 @@ import GoogleMaps
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    override func loadView() {
+        
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.camera = camera
         mapView.isMyLocationEnabled = true
-        view = mapView
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
@@ -36,6 +29,6 @@ class ViewController: UIViewController {
         marker.snippet = "Australia"
         marker.map = mapView
     }
-
+    
 }
 
